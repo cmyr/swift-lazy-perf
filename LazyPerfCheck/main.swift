6273d24a389ac32503cae02621d7f6b876c3c82e
Colin Rofls <colin@cmyr.net>
2018-01-10 12:58:01 -0500

    Initial Commit

---
+++ LazyPerfCheck/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LazyPerfCheck
+//
+//  Created by Colin Rofls on 2018-01-10.
+//  Copyright © 2018 Colin Rofls. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
